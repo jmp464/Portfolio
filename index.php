b85c9e5aadd30192cf1755732ad1c93174abfda3
jmp464 <jmp464@cornell.edu>
2015-01-27 10:44:46 -0500

    Added all files

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>James Palmer</title>
+<meta charset="UTF-8">
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div id="top"></div>
+	<div id="nav">
+		<ul>
+			<li><a href="#top">Home</a></li>
+			<li><a href="#two">Portfoloio</a></li>
+			<li><a href="#three">About</a></li>
+		</ul>
+	</div>
+	<div id="one">
+		<h1 id="me">James Palmer</h1>
+		<!-- <img id="logo" src="images/ghost.jpg" alt="J.M. Palmer"> -->
+		<h2 id="tag"> Creativity is my passion.</h2>
+	</div>
+	<div id="two">
+		<div id="work">
+			<h1>Portfolio<h1>
+		</div>
+			<div id="sites">
+				<div class="site1">
+				<a href="yumyuk.php"><h2>Yum Yuk: Hackathon Finalist</h2></a>
+				<p>I headed up the design and creative direction on this Big Red Hacks
+					finalist.</p>
+				<a href="yumyuk.php"><img src="images/Yum-Yuk.png" alt=""></a>
+				<a href="jampixel.php"><h2>Jampixel: Personal Site</h2></a>
+				<p>I designed this site to showcase some pixel art I was
+					making.</p>
+				<a href="jampixel.php"><img src="images/jampixel.png" alt=""></a>
+				</div>
+				<div class="site2">
+				<a href="apple.php"><h2>Ithaca Apple Fest: Site Redesign</h2></a>
+				<p>A redesign of a local festival's website, including
+				responsive design.</p>
+				<a href="apple.php"><img src="images/Apple.png" alt=""></a>
+				<a href="lmt.php"><h2>Brittany Rose LMT: Client Site</h2></a>
+				<p>I worked on a team and primarily did jquery and responsive 
+					design.</p>
+				<a href="lmt.php"><img src="images/LMT.png" alt=""></a>
+				</div>
+			</div>
+	</div>
+	<div id="three">
+		<h1>About Me</h1>
+		<p>Hi! My name is James and I’m an Information Science student 
+			at Cornell.  I was born in Illinois but grew up in Ithaca, 
+			NY.  Because of a sibling rivalry with my slightly older 
+			brother I wanted to distinguish myself. And so I 
+			decided to be creative. I delved into poetry and theatre, 
+			and just in general thinking outside the box.
+		</p>
+		<p>
+			At present day I’m an actor, poet, avid video game player, 
+			artist, activist, and web developer.  I’m seeking to get 
+			involved in cool web design, game design, and software projects as a UX/UI developer 
+			and/or project manager.
+		</p>
+	</div>
+	
+
+</body>
+</html>
---
+++ jampixel.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>James Palmer</title>
+<meta charset="UTF-8">
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div id="top"></div>
+	<div id="nav">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="index.php#two">Portfoloio</a></li>
+			<li><a href="index.php#three">About</a></li>
+		</ul>
+	</div>
+	<div id="skinny">
+		<div id="work">
+			<h1>Jampixel<h1>
+		</div>
+			<div id="sites">
+				<div class="site1">
+				<h2>Jampixel: Personal Site</h2>
+				<p>I designed this site to showcase some pixel art I was
+					making.</p>
+				<p> On the commission page I created a php form with extensive client-side
+					and server-side validation.</p>
+				<p> Every last inch of what's in these pics (except for maybe the typography)
+					was designed by yours truly!</p>
+				<div class="pic"><img src="images/jampixel.png" alt=""></div>
+				<div class="pic"><img src="images/j1.png" alt=""></div>
+				<div class="pic"><img src="images/j2.png" alt=""></div>
+				<div class="pic"><img src="images/j3.png" alt=""></div>
+				<div class="pic"><img src="images/j4.png" alt=""></div>
+				
+			</div>
+		</div>
+</body>
+</html>
